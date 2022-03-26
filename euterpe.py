26a716073b5b56cc63ff76e46e07d82351d88980
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2022-03-26 08:27:29 -0700

    Add empty model classes to euterpe.py
    Create empty model classes inheriting tf.keras.model, to be filed out later into actual models obviously.

--- euterpe.py
+++ euterpe.py
@@ -1,1 +1,33 @@
-print("Hi, I am euterpe")
+import tensorflow as tf
+
+class EuterpeModelRNN(tf.keras.Model):
+
+  def __init__(self):
+    super(EuterpeModelRNN, self).__init__()
+
+  def call(self, x):
+    return x
+
+class EuterpeModelLSTM(tf.keras.Model):
+  
+  def __init__(self):
+    super(EuterpeModelLSTM, self).__init()
+
+  def call(self, x):
+    return x
+
+class EuterpeModelAutoEncoder(tf.keras.Model):
+
+  def __init__(self):
+    super(EuterpeModelAutoEncoder, self).__init()
+
+  def call(self, x):
+    return x
+  
+class EuterpeModelTransformer(tf.keras.Model):
+
+  def __init__(self):
+    super(EuterpeModelTransformer, self).__init__()
+
+  def call(self, x):
+    pass
