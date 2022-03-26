8f1853d2d7c6a75b26b1cbcc2c7905a97d4c2ffc
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2022-03-26 14:24:28 -0700

    Add OneStep class for LSTM and RNN euterpe models.

--- Models/euterpe.py
+++ Models/euterpe.py
@@ -28,6 +28,33 @@ def get_lstm_layer(rnnu, return_sequences=True, recurrent_initializer='glorot_un
     )
     return lstm
 
+class OneStep(tf.keras.Model):
+  def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
+    super().__init__()
+    self.temperature = temperature
+    self.model = model
+    self.chars_from_ids = chars_from_ids
+    self.ids_from_chars = ids_from_chars
+
+    skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
+    sparse_mask = tf.SparseTensor(
+        values=[-float('inf')]*len(skip_ids),
+        indices=skip_ids,
+        dense_shape=[len(ids_from_chars.get_vocabulary())])
+    self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+
+  @tf.function
+  def generate_one_step(self, inputs, states=None):
+    input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
+    input_ids = self.ids_from_chars(input_chars).to_tensor()
+    predicted_logits, states = self.model(inputs=input_ids, states=states, return_state=True)
+    predicted_logits = predicted_logits[:, -1, :]
+    predicted_logits = predicted_logits/self.temperature
+    predicted_logits = predicted_logits + self.prediction_mask
+    predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+    predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+    predicted_chars = self.chars_from_ids(predicted_ids)
+    return predicted_chars, states
 
 class EuterpeModelRNN(tf.keras.Model):
 
