6c839f14690a6d2ee4810988824eb106e34f3ddf
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2022-03-26 19:01:56 -0700

    Create seq2seq.py

---
+++ Models/seq2seq.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+from tensorflow import ShapeChecker
+
+class ShapeChecker():
+  def __init__(self):
+    self.shapes = {}
+
+  def __call__(self, tensor, names, broadcast=False):
+    if not tf.executing_eagerly():
+      return
+
+    if isinstance(names, str):
+      names = (names,)
+
+    shape = tf.shape(tensor)
+    rank = tf.rank(tensor)
+
+    if rank != len(names):
+      raise ValueError(f'Rank mismatch:\n'
+                       f'    found {rank}: {shape.numpy()}\n'
+                       f'    expected {len(names)}: {names}\n')
+
+    for i, name in enumerate(names):
+      if isinstance(name, int):
+        old_dim = name
+      else:
+        old_dim = self.shapes.get(name, None)
+      new_dim = shape[i]
+
+      if (broadcast and new_dim == 1):
+        continue
+
+      if old_dim is None:
+        # If the axis name is new, add its length to the cache.
+        self.shapes[name] = new_dim
+        continue
+
+      if new_dim != old_dim:
+        raise ValueError(f"Shape mismatch for dimension: '{name}'\n"
+                         f"    found: {new_dim}\n"
+                         f"    expected: {old_dim}\n")
+
+class Encoder(tf.keras.layers.Layer):
+  def __init__(self, input_vocab_size, embedding_dim, enc_units):
+    super(Encoder, self).__init__()
+    self.enc_units = enc_units
+    self.input_vocab_size = input_vocab_size
+
+    self.embedding = tf.keras.layers.Embedding(self.input_vocab_size,
+                                               embedding_dim)
+    self.gru = tf.keras.layers.GRU(self.enc_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+
+  def call(self, tokens, state=None):
+    shape_checker = ShapeChecker()
+    shape_checker(tokens, ('batch', 's'))
+
+    vectors = self.embedding(tokens)
+    shape_checker(vectors, ('batch', 's', 'embed_dim'))
+
+    output, state = self.gru(vectors, initial_state=state)
+    shape_checker(output, ('batch', 's', 'enc_units'))
+    shape_checker(state, ('batch', 'enc_units'))
+
+    return output, state
