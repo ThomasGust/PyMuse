563fff93c130549b108c1d41da7414854cb79238
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2022-03-26 19:07:47 -0700

    Update seq2seq.py

--- Models/seq2seq.py
+++ Models/seq2seq.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import ShapeChecker
-
+import typing
+from typing import Any, Tuple
 class ShapeChecker():
   def __init__(self):
     self.shapes = {}
@@ -65,3 +66,68 @@ class Encoder(tf.keras.layers.Layer):
     shape_checker(state, ('batch', 'enc_units'))
 
     return output, state
+  
+class BahdanauAttention(tf.keras.layers.Layer):
+  def __init__(self, units):
+    super().__init__()
+    # For Eqn. (4), the  Bahdanau attention
+    self.W1 = tf.keras.layers.Dense(units, use_bias=False)
+    self.W2 = tf.keras.layers.Dense(units, use_bias=False)
+
+    self.attention = tf.keras.layers.AdditiveAttention()
+
+  def call(self, query, value, mask):
+    shape_checker = ShapeChecker()
+    shape_checker(query, ('batch', 't', 'query_units'))
+    shape_checker(value, ('batch', 's', 'value_units'))
+    shape_checker(mask, ('batch', 's'))
+
+    w1_query = self.W1(query)
+    shape_checker(w1_query, ('batch', 't', 'attn_units'))
+
+    w2_key = self.W2(value)
+    shape_checker(w2_key, ('batch', 's', 'attn_units'))
+
+    query_mask = tf.ones(tf.shape(query)[:-1], dtype=bool)
+    value_mask = mask
+
+    context_vector, attention_weights = self.attention(
+        inputs = [w1_query, value, w2_key],
+        mask=[query_mask, value_mask],
+        return_attention_scores = True,
+    )
+    shape_checker(context_vector, ('batch', 't', 'value_units'))
+    shape_checker(attention_weights, ('batch', 't', 's'))
+
+    return context_vector, attention_weights
+
+class Decoder(tf.keras.layers.Layer):
+  def __init__(self, output_vocab_size, embedding_dim, dec_units):
+    super(Decoder, self).__init__()
+    self.dec_units = dec_units
+    self.output_vocab_size = output_vocab_size
+    self.embedding_dim = embedding_dim
+
+    self.embedding = tf.keras.layers.Embedding(self.output_vocab_size,
+                                               embedding_dim)
+
+    self.gru = tf.keras.layers.GRU(self.dec_units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+
+    self.attention = BahdanauAttention(self.dec_units)
+
+    self.Wc = tf.keras.layers.Dense(dec_units, activation=tf.math.tanh,
+                                    use_bias=False)
+
+    self.fc = tf.keras.layers.Dense(self.output_vocab_size)
+
+class DecoderInput(typing.NamedTuple):
+  new_tokens: Any
+  enc_output: Any
+  mask: Any
+
+class DecoderOutput(typing.NamedTuple):
+  logits: Any
+  attention_weights: Any
