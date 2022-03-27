cd9e549336926c50bcb0bb0ed8face9454afee79
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2022-03-26 19:25:11 -0700

    Delete Useless files

---
+++ Models/utils.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+def gram_matrix(input_tensor):
+    result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+    input_shape = tf.shape(input_tensor)
+    num_locations = tf.cast(input_shape[1] * input_shape[2], tf.float32)
+    return result / num_locations
