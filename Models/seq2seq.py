5b289d78994af8ca309019de90d7bb86701defe8
Thomas Gust <64709003+ThomasGust@users.noreply.github.com>
2022-03-27 15:19:09 -0700

    Add MaskedLoss to seq2seq

--- Models/seq2seq.py
+++ Models/seq2seq.py
@@ -132,3 +132,23 @@ class DecoderInput(typing.NamedTuple):
 class DecoderOutput(typing.NamedTuple):
   logits: Any
   attention_weights: Any
+
+class MaskedLoss(tf.keras.losses.Loss):
+  def __init__(self):
+    self.name = 'masked_loss'
+    self.loss = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
+
+  def __call__(self, y_true, y_pred):
+    shape_checker = ShapeChecker()
+    shape_checker(y_true, ('batch', 't'))
+    shape_checker(y_pred, ('batch', 't', 'logits'))
+
+    loss = self.loss(y_true, y_pred)
+    shape_checker(loss, ('batch', 't'))
+
+    mask = tf.cast(y_true != 0, tf.float32)
+    shape_checker(mask, ('batch', 't'))
+    loss *= mask
+
+    return tf.reduce_sum(loss)
