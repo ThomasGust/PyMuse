4180ddeb11f5a41b582ee7dda572728de80a4229
TGSAAS <102374339+TGSAAS@users.noreply.github.com>
2022-03-28 13:19:14 -0700

    Add BahdanauAttention class to attentions.py

--- Layers/attentions.py
+++ Layers/attentions.py
@@ -1,2 +1,35 @@
 import tensorflow as tf
 import numpy as np
+
+class BahdanauAttention(tf.keras.layers.Layer):
+  def __init__(self, units):
+    super().__init__()
+    self.W1 = tf.keras.layers.Dense(units, use_bias=False)
+    self.W2 = tf.keras.layers.Dense(units, use_bias=False)
+
+    self.attention = tf.keras.layers.AdditiveAttention()
+
+  def call(self, query, value, mask):
+    shape_checker = ShapeChecker()
+    shape_checker(query, ('batch', 't', 'query_units'))
+    shape_checker(value, ('batch', 's', 'value_units'))
+    shape_checker(mask, ('batch', 's'))
+
+    w1_query = self.W1(query)
+    shape_checker(w1_query, ('batch', 't', 'attn_units'))
+
+    w2_key = self.W2(value)
+    shape_checker(w2_key, ('batch', 's', 'attn_units'))
+
+    query_mask = tf.ones(tf.shape(query)[:-1], dtype=bool)
+    value_mask = mask
+
+    context_vector, attention_weights = self.attention(
+        inputs = [w1_query, value, w2_key],
+        mask=[query_mask, value_mask],
+        return_attention_scores = True,
+    )
+    shape_checker(context_vector, ('batch', 't', 'value_units'))
+    shape_checker(attention_weights, ('batch', 't', 's'))
+
+    return context_vector, attention_weights
