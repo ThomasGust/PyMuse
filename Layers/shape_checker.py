7a3b8f632415ed707d283e5421b5b7e0ad52d78b
TGSAAS <102374339+TGSAAS@users.noreply.github.com>
2022-03-28 13:11:37 -0700

    Create shape_checker.py

---
+++ Layers/shape_checker.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+
+class ShapeChecker():
+  def __init__(self):
+    self.shapes = {}
+
+  def __call__(self, tensor, names, broadcast=False):
+    if not tf.executing_eagerly():
+      return
+
+    if isinstance(names, str):
+      names = (names,)
+
+    shape = tf.shape(tensor)
+    rank = tf.rank(tensor)
+
+    if rank != len(names):
+      raise ValueError(f'Rank mismatch:\n'
+                       f'    found {rank}: {shape.numpy()}\n'
+                       f'    expected {len(names)}: {names}\n')
+
+    for i, name in enumerate(names):
+      if isinstance(name, int):
+        old_dim = name
+      else:
+        old_dim = self.shapes.get(name, None)
+      new_dim = shape[i]
+
+      if (broadcast and new_dim == 1):
+        continue
+
+      if old_dim is None:
+        self.shapes[name] = new_dim
+        continue
+
+      if new_dim != old_dim:
+        raise ValueError(f"Shape mismatch for dimension: '{name}'\n"
+                         f"    found: {new_dim}\n"
+                         f"    expected: {old_dim}\n")
